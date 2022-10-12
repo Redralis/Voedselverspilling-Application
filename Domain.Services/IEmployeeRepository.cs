e5cf757d8cbf960ea7749f1ef3751a34fb1e869b
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-10-12 10:04:41 +0200

    Made searching and deleting employees work with Id instead of Email.

--- Domain.Services/IEmployeeRepository.cs
+++ Domain.Services/IEmployeeRepository.cs
@@ -4,10 +4,10 @@ public interface IEmployeeRepository
 {
     public Employee CreateEmployee(Employee employee);
     
-    public Employee GetEmployee(string email);
+    public Employee GetEmployee(int id);
 
     public Employee EditEmployee(Employee employee);
 
-    public Employee DeleteEmployee(string email);
+    public Employee DeleteEmployee(int id);
     
 }
