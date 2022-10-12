9621d577ac0561b2ef785ad8a508c167cb8c8cdd
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-10-12 07:08:45 +0000

    Added 6 files to /Domain

---
+++ Domain/Canteen.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain;
+
+public class Canteen
+{
+    public int Id { get; set; }
+
+    public string City { get; set; } = string.Empty;
+    
+    public string Address { get; set; } = string.Empty;
+    
+    public bool ServesWarmMeals { get; set; }
+}
---
+++ Domain/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain;
+
+public class Student
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string DateOfBirth { get; set; } = string.Empty;
+
+    public string StudentNr { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string City { get; set; } = string.Empty;
+
+    public string PhoneNr { get; set; } = string.Empty;
+}
