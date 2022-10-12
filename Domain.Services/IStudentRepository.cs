141b2423db130395da0a4393923490d27b4ff097
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-10-12 10:03:49 +0200

    Renamed incorrectly named file

---
+++ Domain.Services/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Services;
+
+public interface IStudentRepository
+{
+    public Student CreateStudent(Student student);
+    
+    public Student GetStudent(int id);
+
+    public Student EditStudent(Student student);
+
+    public Student DeleteStudent(int id);
+    
+}
